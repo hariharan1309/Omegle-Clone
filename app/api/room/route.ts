fcb033eeac13dbdcbf1d0ce7356b70f76708c0c6
hariharan1309 <hariharana1309@gmail.com>
2024-05-28 15:38:56 +0530

    Route

--- app/api/room/route.ts
+++ app/api/room/route.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 import {dbConnect} from '../../../utils/dbConnect';
 import Room from '../../../models/Room';
 
